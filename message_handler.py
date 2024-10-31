f77ac7cbcc1e417f6940683c0a9a6b60b3aae2d7
startde <rudash.ak@gmail.com>
2024-10-31 12:03:04 +0300

    Обновляю веткку (#1)
    * Обновил функционал Чат-бота
    
    * Обновил функционал Чат-бота 2
    
    ---------
    
    Co-authored-by: Alekhandr0 <alexander.akimiov@yandex.ru>

--- message_handler.py
+++ message_handler.py
@@ -35,7 +35,7 @@ class MessageHandler:
         self.bot.send_message(chat_id, welcome_message)
         self.logger.info(f"Отправлено приветственное сообщение пользователю ID: {user_id}")
 
-    def send_multiple_photos_with_text(self, chat_id, text, photos, url_doc, reply_markup):
+    def send_multiple_photos_with_text(self, chat_id, text, photos, url_doc, type_question_text, question_text ,reply_markup):
             """
             Отправляет несколько фото в одном сообщении с текстом и URL документа.
             """
@@ -43,14 +43,18 @@ class MessageHandler:
             
             # Добавляем текст к первому фото, если он задан
             if media_group:
-                media_group[0].caption = f"{text}"
+                media_group[0].caption = f"{question_text} \n {text}"
             
-            # Отправляем альбом фото
-            self.bot.send_media_group(chat_id, media_group)
-            # Отправляем клавиатуру отдельно после альбома
+            if len(media_group)>0:
+                # Отправляем альбом фото
+                self.bot.send_media_group(chat_id, media_group)
+                # Отправляем клавиатуру отдельно после альбома
+            else:
+                self.bot.send_message(chat_id, text=f"{question_text} \n {text}")    
+
             self.bot.send_message(chat_id, text=f"Ссылка на документ:{url_doc}", reply_markup=reply_markup)
 
-    def create_keyboard(self, faq_mode=False):
+    def create_keyboard(self, faq_mode=False, type_port=0):
         """
         Создает клавиатуру.
         Если faq_mode=True, то добавляются кнопки для режима FAQ.
@@ -58,15 +62,52 @@ class MessageHandler:
         markup = ReplyKeyboardMarkup(resize_keyboard=True)
         
         if faq_mode:
-            # Кнопки для режима FAQ
-            back_button = KeyboardButton("Назад")
-            option1_button = KeyboardButton("1")
-            option2_button = KeyboardButton("2")
-            option3_button = KeyboardButton("3")
-            option4_button = KeyboardButton("4")
-            option5_button = KeyboardButton("5")
-            option6_button = KeyboardButton("6")
-            markup.add(back_button, option1_button, option2_button, option3_button, option4_button, option5_button, option6_button)
+            if type_port==0:
+                back_main = KeyboardButton("Главное меню")  #Диспетчерская Конфигуратор Управление доступом
+                dispetch =  KeyboardButton("Диспетчерская")
+                konfig =  KeyboardButton("Конфигуратор")
+                uprl_dost =  KeyboardButton("Управление доступом")
+                markup.add(back_main, dispetch, konfig, uprl_dost)
+            elif type_port==1:
+            # Кнопки для режима Диспетчерская
+                back_main = KeyboardButton("Главное меню")
+                back_button = KeyboardButton("Назад")
+                option1_button = KeyboardButton("11")
+                option2_button = KeyboardButton("12")
+                option3_button = KeyboardButton("13")
+                option4_button = KeyboardButton("14")
+                option5_button = KeyboardButton("15")
+                option6_button = KeyboardButton("16")
+                option7_button = KeyboardButton("17")
+                option8_button = KeyboardButton("18")
+                option9_button = KeyboardButton("19")
+                markup.add(back_main, back_button, option1_button, option2_button, option3_button, option4_button, option5_button, option6_button, option7_button, option8_button, option9_button)
+            elif type_port==2:
+            # Кнопки для режима Конфигуратор
+                back_main = KeyboardButton("Главное меню")
+                back_button = KeyboardButton("Назад")
+                option21_button = KeyboardButton("21")
+                option22_button = KeyboardButton("22")
+                option23_button = KeyboardButton("23")
+                option24_button = KeyboardButton("24")
+                option25_button = KeyboardButton("25")
+                option26_button = KeyboardButton("26")
+                markup.add(back_main, back_button, option21_button, option22_button, option23_button, option24_button, option25_button, option26_button)
+
+            elif type_port==3:
+            # Кнопки для режима Диспетчерская
+                back_main = KeyboardButton("Главное меню")
+                back_button = KeyboardButton("Назад")
+                option31_button = KeyboardButton("31")
+                option32_button = KeyboardButton("32")
+                option33_button = KeyboardButton("33")
+                option34_button = KeyboardButton("34")
+                option35_button = KeyboardButton("35")
+                option36_button = KeyboardButton("36")
+                option37_button = KeyboardButton("37")
+                option38_button = KeyboardButton("38")
+                markup.add(back_main, back_button, option31_button, option32_button, option33_button, option34_button, option35_button, option36_button, option37_button, option38_button)
+            
         else:
             # Основные кнопки
             clear_button = KeyboardButton("Очистить историю")
@@ -81,6 +122,7 @@ class MessageHandler:
     def handle_message(self, message):
         user_id = message.from_user.id
         user_query = message.text
+        id_question = [str(item["id"]) for item in self.data]
 
         if user_id not in self.chat_history:
             self.chat_history[user_id] = []
@@ -108,27 +150,48 @@ class MessageHandler:
             reply_markup = self.create_keyboard()
 
         elif user_query == "Часто задаваемые вопросы":
-            response_text = "Выберите вопрос, на который хотите получить ответ, или вернитесь назад. \n 1) Как настроить визуализацию на портале \"Диспетчерская\"? \n 2) Как настроить панель на главной странице? \n 3) Как настраивать и изменять фильтры по событиям? \n 4) Как настраивать вид таблицы и применять фильтры? \n 5) Как настраивать вид графиков? \n 6) Как настраивать карту и применять к ней фильтры?"
+            response_text = "Выберите раздел который вас интресует"
             reply_markup = self.create_keyboard(faq_mode=True)  # Переход в режим FAQ
 
         elif user_query == "Назад":
-            response_text = "Вы вернулись в основное меню."
+            response_text = "Вы вернулись в раздел с часто задаваемыми вопросами, тут можно выбрать портал."
+            reply_markup = self.create_keyboard(faq_mode=True)  # Возвращаемся в основное меню
+            
+            # Главное меню
+        
+        elif user_query == "Главное меню":
+            response_text = "Вы вернулись на Главное меню."
             reply_markup = self.create_keyboard()  # Возвращаемся в основное меню
+            
 
-        elif user_query == "1"  or user_query == "2" or user_query == "3" or user_query == "4" or user_query == "5" or user_query == "6" :
+        # elif user_query == "1"  or user_query == "2" or user_query == "3" or user_query == "4" or user_query == "5" or user_query == "6" :
+        # elif ((user_query%10)>0):
+        elif str(user_query) in id_question:
             faq_data = self.get_data_by_id(user_query)
             if faq_data:
                 response_text = faq_data["message"]
                 photos = faq_data["media_group"]
                 url_doc = faq_data["url"]
-                reply_markup = self.create_keyboard(faq_mode=True)
-                self.send_multiple_photos_with_text(message.chat.id, response_text, photos, url_doc, reply_markup)
+                type_question_text =  faq_data["type_question"]
+                question_text = faq_data["question"]
+                reply_markup = self.create_keyboard(faq_mode=True,type_port=int(user_query)//10)
+                self.send_multiple_photos_with_text(message.chat.id, response_text, photos, url_doc, type_question_text, question_text, reply_markup)
                 return
             else:
                 response_text = "Извините, информация по данному вопросу не найдена."
                 reply_markup = self.create_keyboard()
 
-        
+        elif user_query == "Диспетчерская":
+            response_text = "Часто задаваемые вопросы по порталу \"Диспетчерская\": \n11) Как настроить визуализацию на портале \"Диспетчерская\"?\n12) Как настроить панель на главной странице?\n13) Как настраивать и изменять фильтры по событиям?\n14) Как настраивать вид таблицы и применять фильтры?\n15) Как настраивать вид графиков?\n16) Как настраивать карту и применять к ней фильтры?\n17) Как создавать отчеты?\n18) Как создавать графики в отчетах?\n19) Как добавлять и редактировать список контактов для рассылки?"
+            reply_markup = self.create_keyboard(faq_mode=True, type_port=1)  # Переход на вопросы по диспетчерской
+
+        elif user_query == "Конфигуратор":
+            response_text = "Часто задаваемые вопросы по порталу \"Конфигуратор\" \n21) Как настроить оборудование систем телеметрии для передачи данных на пульт управления?\n22) Как удалить параметр из объекта?\n23) Как связать параметр с объектом?\n24) Можно ли использовать один драйвер для нескольких объектов?\n25) Как влияет изменение адресации на привязку параметров к устройству?\n26) Как перезапустить драйвер?"
+            reply_markup = self.create_keyboard(faq_mode=True, type_port=2)  # Переход на вопросы по Конфигуратор
+
+        elif user_query == "Управление доступом":
+            response_text = "Часто задаваемые вопросы по порталу \"Управление доступом\" \n31) Как создать новый аккаунт?\n32) Как редактировать информацию об аккаунте?\n33) Какие правила доступа можно установить для пользователей?\n34) Как создать пользователей с необходимыми ролями?\n35) Как создать пользователей с необходимыми ролями?\n36) Как настроить доступ к системе с использованием RBAC и ABAC в Акси.SCADA?\n37) Как создать новый профиль\n38) Можно ли создавать собственные роли пользователей?"
+            reply_markup = self.create_keyboard(faq_mode=True, type_port=3)  # Переход на вопросы по Управление доступом    
 
         else:
             # Обработка пользовательских запросов, как и раньше
